47a89425ead74f62f36e90ecdba9e6de3804b472
BradleyGebhardt <39223742+BradleyGebhardt@users.noreply.github.com>
2019-08-29 13:30:36 +0200

    Added password hashing

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,82 @@
 var express = require('express');
-var router = express.Router({mergeParams: true});
+var router = express.Router({ mergeParams: true });
 let score = require('../models/score');
-let user = require('../models/user');
+var scoresList;
+let User = require('../models/user');
+var { check, validationResult, oneOf } = require('express-validator');
 
 /* GET home page. */
 router.get('/', function (req, res) {
-  req.session.test = 'test';
   score.find({}, (err, scores) => {
     if (err) {
       console.log(err);
     } else {
-      res.render('index', { 
+      scoresList = scores;
+      res.render('index', {
         title: 'Four score and a lot of time',
         list: scores
       });
+      console.log(req.session.user);
     }
   }).sort('user')
     .sort('sport')
-      .sort({score: 'desc'});
+    .sort({ score: 'desc' });
+});
+
+router.route('/login').get((req, res) => {
+  res.render('login');
+});
+
+router.route('/login').post([
+  check('username').exists({ checkFalsy: true, checkNull: true }).withMessage('Username is required'),
+  check('password').exists({ checkFalsy: true, checkNull: true }).withMessage('Password is required')
+], (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    res.render('login', {
+      errors: errors.array()
+    });
+  } else {
+    User.findOne({username: req.body.username, password: req.body.password}, (err, user) => {
+      if(err) {
+        console.log(err);
+      } else {
+        if(user) {
+          req.session.user = user.username;
+          res.redirect('/');
+        } else {
+          req.flash('danger', 'Username or password is incorrect');
+          res.render('login');
+        }
+      }
+    });
+    // User.find({}, (err, user) => {
+    //   if (err) {
+    //     console.log(err);
+    //   } else {
+    //     if (user.length != 0) {
+    //       req.session.user = req.body.username;
+    //       console.log(req.body.username);
+    //       score.find({}, (err, scores) => {
+    //         if (err) {
+    //           console.log(err);
+    //         } else {
+    //           scoresList = scores;
+    //           res.render('index', {
+    //             title: 'Four score and a lot of time',
+    //             list: scores
+    //           });
+    //         }
+    //       })
+    //         .sort('user')
+    //         .sort('sport')
+    //         .sort({ score: 'desc' });
+    //     } else {
+    //       req.flash('danger', 'Username or password is incorrect');
+    //     }
+    //   }
+    // });
+  }
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router({ mergeParams: true });
-let user = require('../models/user');
+let User = require('../models/user');
 let Score = require('../models/score');
+const bcrypt = require('bcryptjs');
 var db = require('mongoose');
+var { check, validationResult, oneOf } = require('express-validator');
+var allowed;
 // var id = require('../public/js/main');
 
 /* GET users listing. */
@@ -11,9 +14,14 @@ router.route('/:user/:id').get(function (req, res, next) {
     if (err) {
       console.log(err)
     } else {
-      console.log(found._id);
+      if (req.session.user == found.user) {
+        allowed = true;
+      } else {
+        allowed = false;
+      }
       res.render('user', {
-        userInfo: found
+        userInfo: found,
+        signedInUser: allowed
       });
     }
   });
@@ -35,4 +43,51 @@ router.route('/:user/:id').delete((req, res) => {
 router.route('/register').get((req, res) => {
   res.render('register');
 })
+
+router.route('/register').post([
+  check('username').exists({ checkNull: true, checkFalsy: true }).withMessage('Username is a required field'),
+  check('password').exists({ checkNull: true, checkFalsy: true }).withMessage('Password is a required filed'),
+  check('password').isLength({ min: 8, max: 15 }).withMessage('Password is not the right length (min: 8 and max: 15)'),
+  check('email').exists({ checkNull: true, checkFalsy: true }).isEmail().withMessage('Email is a required field')
+], (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    if (req.body.password != req.body.password2) {
+      req.flash('danger', 'Passwords do not match');
+    }
+    res.render('register', {
+      errors: errors.array()
+    });
+  } else {
+    var user = new User({
+      username: req.body.username,
+      password: req.body.password2,
+      email: req.body.email
+    });
+
+    bcrypt.genSalt(10, (err, salt) => {
+      if (err) {
+        console.log(err);
+      } else {
+        bcrypt.hash(user.password, salt, (err, hash) => {
+          if (err) {
+            console.log(err);
+          } else {
+            user.password = hash;
+            user.save()
+              .then(user => {
+                req.flash('success', 'Added new user and you may now login');
+                res.redirect('/login');
+              })
+              .catch(err => {
+                res.status(400).send("unable to save to database");
+              })
+          }
+        });
+      }
+    });
+
+  }
+});
 module.exports = router;
